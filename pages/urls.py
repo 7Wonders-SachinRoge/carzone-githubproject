339ad5b06e5658244aaf36f57186e68a38ea4e60
sachin11-the-only <sachin.roge11@gmail.com>
2021-09-10 13:07:19 +0530

    Created pages app and configured navigation links

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = "pages"
+
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('service/', views.service, name='service'),
+    path('contact/', views.contact, name='contact'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'pages/home.html')
+
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+
+def service(request):
+    return render(request, 'pages/service.html')
+
+
+def contact(request):
+    return render(request, 'pages/contact.html')
